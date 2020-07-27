5d107884908d7a522a3de73f7ff41ead6d2b3127
Kristine Du <kristine.a.du@gmail.com>
2020-07-27 16:16:08 -0600

    Add user resource.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_212012) do
+ActiveRecord::Schema.define(version: 2020_07_27_211619) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.boolean "urgent"
     t.boolean "done"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
